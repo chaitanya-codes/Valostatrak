99b8d8b459470b7f8570e304cc0f46c2245a2c66
Glitch (valostatrak) <none>
2023-05-04 17:41:45 +0000

    🛁🔢 Updated with Glitch

--- src/commands/account.js
+++ src/commands/account.js
@@ -21,16 +21,52 @@ module.exports.execute = async (client, message, args, send) => {
 	if (client.accounts.has(args.join(" ").toLowerCase())) region = client.accounts.get(name.toLowerCase())
 		else return client.newUser(args.join(" "), this.info.name, message, subcommand)
 
-			if (subcommand === 'find') {
-				if (!args.join(" ").includes("#")) return message.reply("Account not found. Usage: `/account <name#tagg>`\nExample: `/account 100T Asuna#1111`")
-					if (tag.split(" ")[1]) return message.reply("You don't have to include the region for this command")
-						
-						let mm;
-					let wait = new Discord.EmbedBuilder()
-					.setColor(428985)
-					.setTitle("Searching...")
-					await send(message, {reply: true, embeds: [wait]})
-					.then(m => mm = m)
+	if (subcommand === 'find') {
+		if (!args.join(" ").includes("#")) return message.reply("Account not found. Usage: `/account <name#tagg>`\nExample: `/account 100T Asuna#1111`")
+			if (tag.split(" ")[1]) return message.reply("You don't have to include the region for this command")
+				
+				let mm;
+			let wait = new Discord.EmbedBuilder()
+			.setColor(428985)
+			.setTitle("Searching...")
+			await send(message, {reply: true, embeds: [wait]})
+			.then(m => mm = m)
+			require('request')(`http://api.henrikdev.xyz/valorant/v1/account/${name}/${tag}`, async (err, res, body) => {
+
+				if (err || JSON.parse(body).status !== 200) return send(message, client.notFound(JSON.parse(body).message))
+					let data = JSON.parse(body)
+				data = data.data
+				if (!data) return send(message, client.notFound(JSON.parse(body).message))
+
+					let statEmbed = new Discord.EmbedBuilder()
+				.setColor(342852)
+				.setTitle("Account  - " + name + "#" + tag)
+				.setDescription("**Account Level**: " + data.account_level + "\n**Region**: " + data.region)
+				.setFooter({text: "To view match history, use /matches command"})
+				.setImage(data.card?.large)
+				.setThumbnail(client.levelborderData.filter(border => border.startingLevel == (Math.floor(data.account_level / 20) * 20))[0].levelNumberAppearance || client.levelborderData.filter(border => border.startingLevel ==1)[0].levelNumberAppearance)
+				send(mm, {edit: true, embeds: [statEmbed]})
+			})
+		}
+		else if (subcommand === 'link') {
+			const Discord = require('discord.js')
+			const modal = new Discord.ModalBuilder()
+			.setCustomId('modal')
+			.setTitle('Verification')
+			const actionrow = new Discord.ActionRowBuilder().addComponents([new Discord.TextInputBuilder()
+				.setCustomId("lvl")
+				.setLabel("What is your account level? (verification)")
+				.setStyle(Discord.TextInputStyle.Short)
+				])
+			modal.addComponents([actionrow])
+			let filt = (i) => i.user.id === message.author.id
+			message.showModal(modal)
+			message.awaitModalSubmit({filt, time: 30000})
+			.then(j => {
+				let fields = j.fields.fields
+				let lvl = fields.get('lvl').value
+				if (isNaN(lvl)) return send(j, 'Level entered was not a number!')
+
 					require('request')(`http://api.henrikdev.xyz/valorant/v1/account/${name}/${tag}`, async (err, res, body) => {
 
 						if (err || JSON.parse(body).status !== 200) return send(message, client.notFound(JSON.parse(body).message))
@@ -38,68 +74,34 @@ module.exports.execute = async (client, message, args, send) => {
 						data = data.data
 						if (!data) return send(message, client.notFound(JSON.parse(body).message))
 
-							let statEmbed = new Discord.EmbedBuilder()
-						.setColor(342852)
-						.setTitle("Account  - " + name + "#" + tag)
-						.setDescription("**Account Level**: " + data.account_level + "\n**Region**: " + data.region)
-						.setFooter({text: "To view match history, use /matches command"})
-						.setImage(data.card?.large)
-						send(mm, {edit: true, embeds: [statEmbed]})
-					})
-				}
-				else if (subcommand === 'link') {
-					const Discord = require('discord.js')
-					const modal = new Discord.ModalBuilder()
-					.setCustomId('modal')
-					.setTitle('Verification')
-					const actionrow = new Discord.ActionRowBuilder().addComponents([new Discord.TextInputBuilder()
-						.setCustomId("lvl")
-						.setLabel("What is your account level? (verification)")
-						.setStyle(Discord.TextInputStyle.Short)
-						])
-					modal.addComponents([actionrow])
-					let filt = (i) => i.user.id === message.author.id
-					message.showModal(modal)
-					message.awaitModalSubmit({filt, time: 30000})
-					.then(j => {
-						let fields = j.fields.fields
-						let lvl = fields.get('lvl').value
-						if (isNaN(lvl)) return send(j, 'Level entered was not a number!')
-
-							require('request')(`http://api.henrikdev.xyz/valorant/v1/account/${name}/${tag}`, async (err, res, body) => {
-
-								if (err || JSON.parse(body).status !== 200) return send(message, client.notFound(JSON.parse(body).message))
-									let data = JSON.parse(body)
-								data = data.data
-								if (!data) return send(message, client.notFound(JSON.parse(body).message))
-
-									if (data.account_level===Number(lvl)) {
-										client.linked.set(message.author.id, {nametag: args.join(" ").toLowerCase(), private: false})
-										send(j, {embeds: [new Discord.EmbedBuilder().setColor("Green").setTitle("Linked account").setDescription("Your discord account has been linked to the valorant account `" + name + "`")]})
-									} else return send(j, {embeds: [new Discord.EmbedBuilder().setTitle("Failed verification").setDescription("Account level did not match").setColor("Red")]})
-								})
-					})
-				} 
-				else if (subcommand === 'settings') {
-					let linked = client.linked.get(message.author.id)
-					if (!linked) return send(message, "You have not linked your valorant account with the bot!")
-						const row = new Discord.ActionRowBuilder().addComponents([new Discord.ButtonBuilder().setCustomId("private").setLabel("Statistics are public").setStyle("Primary"), new Discord.ButtonBuilder().setCustomId("remove").setLabel("Remove account from bot").setStyle("Secondary")])
-					const emb = new Discord.EmbedBuilder().setTitle("Account Settings").setDescription("Statistics by default are set to be public which allows anyone to view your account info, however you can turn this off to only let you see your account statistics.\nYou can re-link your account by removing the linked account if you edited username.")
-					if (linked.private) row.components[0].setLabel("Statistics are private").setStyle("Danger")
-						let m = await send(message, {embeds: [emb], components: [row]})
-					let filt = (i) => i.user.id === message.author.id
-					let col = m.createMessageComponentCollector({filt, time: 30000})
-					col.on("collect", async i => {
-						if (i.customId === 'private') {
-							if (!linked.private) client.linked.set(message.author.id, true, 'private')
-								else await client.linked.set(message.author.id, false, 'private')
-									linked = client.linked.get(message.author.id)
-								send(i, 'Changed your linked account statistics to: ' + (linked.private ? '`PRIVATE`' : '`PUBLIC`'))
-							}
-							else if (i.customId === 'remove') {
-								client.accounts.delete(client.linked.get(message.author.id, 'nametag'))
-								send(i, 'Removed account from the bot database!')
-							}
+							if (data.account_level===Number(lvl)) {
+								client.linked.set(name+"#"+tag, {id: message.author.id, private: false})
+								send(j, {embeds: [new Discord.EmbedBuilder().setColor("Green").setTitle("Linked account").setDescription("Your discord account has been linked to the valorant account `" + name + "`")]})
+							} else return send(j, {embeds: [new Discord.EmbedBuilder().setTitle("Failed verification").setDescription("Account level did not match").setColor("Red")]})
 						})
+			})
+		} 
+		else if (subcommand === 'settings') {
+			let nametag = name+'#'+tag
+			let linked = client.linked.get(nametag)
+			if (linked.id !== message.author.id) return send(message, "You have not linked your valorant account with the bot!")
+				const row = new Discord.ActionRowBuilder().addComponents([new Discord.ButtonBuilder().setCustomId("private").setLabel("Statistics are public").setStyle("Primary"), new Discord.ButtonBuilder().setCustomId("remove").setLabel("Remove account from bot").setStyle("Secondary")])
+			if (linked.private) row.components[0].setLabel("Statistics are private").setStyle("Danger")
+				const emb = new Discord.EmbedBuilder().setTitle("Account Settings").setDescription("Statistics by default are set to be public which allows anyone to view your account info, however you can turn this off to only let you see your account statistics.\nYou can re-link your account by removing the linked account if you edited username.")
+			let m = await send(message, {embeds: [emb], components: [row]})
+			let filt = (i) => i.user.id === message.author.id
+			let col = m.createMessageComponentCollector({filt, time: 30000})
+			col.on("collect", async i => {
+				if (i.customId === 'private') {
+					if (!linked.private) client.linked.set(nametag, true, 'private')
+						else await client.linked.set(nametag, false, 'private')
+					linked = client.linked.get(nametag)
+					send(i, 'Changed your linked account statistics to: ' + (linked.private ? '`PRIVATE`' : '`PUBLIC`'))
 				}
-			}
+				else if (i.customId === 'remove') {
+					client.accounts.delete(nametag)
+					send(i, 'Removed account from the bot database!')
+				}
+			})
+		}
+	}
--- src/events/ready.js
+++ src/events/ready.js
@@ -9,17 +9,18 @@ module.exports.Ready = async (client) => {
 	client.wait = (time) => require('util').promisify(setTimeout)(time)
 	client.user.setActivity(`your stats | /help`, { type: ActivityType.Watching })
 
-	let array = ['weapons', 'maps', 'agents', 'sprays', 'weapons/skinlevels', 'bundles', 'buddies', 'playertitles', 'playercards', 'contracts']
+	let array = ['weapons', 'maps', 'agents', 'sprays', 'weapons/skinlevels', 'bundles', 'buddies', 'playertitles', 'playercards', 'contracts', 'weapons/skins', 'levelborders']
 	await array.forEach(async (api, index) => {
 		await client.wait(2500 * index)
 		await request(baseURL + (api === 'agents' ? 'agents?isPlayableCharacter=true' : api), (err, res, body) => {
 			if (api === 'weapons/skinlevels') client['skinLevelData'] = JSON.parse(body).data
-				else if (api === 'buddies') client['buddiesData'] = JSON.parse(body).data
-					else client[api.substring(0, api.length - 1) + "Data"] = JSON.parse(body).data
-						if (api === 'maps') client.commands.get('map').info.choices = client.mapData.map(m => m.displayName)
-							else if (api === 'agents') client.commands.get('agent').info.choices = client.agentData.map(a => a.displayName)
-								else if (api === 'weapons') client.skins = client.weaponData.map(w => {return w.skins.map(a => a.displayName)}).flat(Infinity)
-							})
+				else if (api === 'weapons/skins') client['skinData'] = JSON.parse(body).data
+					else if (api === 'buddies') client['buddiesData'] = JSON.parse(body).data
+						else client[api.substring(0, api.length - 1) + "Data"] = JSON.parse(body).data
+					if (api === 'maps') client.commands.get('map').info.choices = client.mapData.map(m => m.displayName)
+						else if (api === 'agents') client.commands.get('agent').info.choices = client.agentData.map(a => a.displayName)
+							else if (api === 'weapons') client.skins = client.weaponData.map(w => {return w.skins.map(a => a.displayName)}).flat(Infinity)
+						})
 		console.log("Loaded " + api + " into database!")
 	})
 	client.ratelimit = (id) => {
@@ -35,10 +36,10 @@ module.exports.Ready = async (client) => {
 		if (tier === 23) return "https://static.wikia.nocookie.net/valorant/images/5/53/Ascendant_3_Rank.png/revision/latest/scale-to-width-down/250?cb=20220616175519"
 			else if (tier === 22) return "https://static.wikia.nocookie.net/valorant/images/1/1e/Ascendant_2_Rank.png/revision/latest/scale-to-width-down/250?cb=20220616175514"
 				else if (tier === 21) return "https://static.wikia.nocookie.net/valorant/images/e/e5/Ascendant_1_Rank.png/revision/latest/scale-to-width-down/250?cb=20220616175506"
-	if (tier === 24) tier=21
-		else if (tier === 25) tier = 22
-			else if (tier===26) tier= 23
-				else if (tier===27) tier = 24
+					if (tier === 24) tier=21
+						else if (tier === 25) tier = 22
+							else if (tier===26) tier= 23
+								else if (tier===27) tier = 24
 	return `https://raw.githubusercontent.com/RumbleMike/ValorantStreamOverlay/main/Resources/TX_CompetitiveTier_Large_${tier}.png`//client.guilds.cache.get('501396018395480065').emojis.cache.find(e=>e.name===rank.split(' ')[0].toLowerCase()).url || 'https://raw.githubusercontent.com/RumbleMike/ValorantStreamOverlay/main/Resources/TX_CompetitiveTier_Large_0.png'
 }
 }
