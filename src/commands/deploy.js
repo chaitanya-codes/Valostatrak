0d175f53e9e0f9e0ef7dddcfdc79e28342ffab3d
Glitch (valostatrak) <none>
2023-07-21 12:54:52 +0000

    🔴🥏 Updated with Glitch

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -21,7 +21,7 @@ module.exports.execute = async (client, message, args, send) => {
         else if (arg === 'agent') return client.agentData.map(a => {return{name: a.displayName, value: a.displayName}})
           else if (arg === 'leaderboard') return ['global', 'this-server'].map(e => {return{name: e, value: e}})
             else if (arg === 'weapon') return client.weaponData.map(a => {return{name: a.displayName, value: a.displayName}})
-              else if (arg === 'match-type') return ['unrated', 'competitive', 'spikerush', 'deathmatch', 'replication', 'escalation', 'snowball', 'custom'].map(a => {return{name: a, value: a}})
+              else if (arg === 'match-type') return ['unrated', 'competitive', 'spikerush', 'deathmatch', 'teamdeathmatch', 'replication', 'escalation', 'snowballfight', 'swiftplay', 'premier', 'custom'].map(a => {return{name: a, value: a}})
                 else if (arg === 'map') return client.mapData.map(a => {return{name: a.displayName, value: a.displayName}})
                   else if (arg === 'query') return [{name: 'Find an account', value: "find"}, {name: "Link your account", value: 'link'}, {name: 'Change settings for your linked account', value: 'settings'}]
                     else if (arg === 'region-esports') return ['international', 'north america', 'emea', 'brazil', 'japan', 'korea', 'latin_america', 'latin_america_south', 'latin_america_north', 'southeast_asia', 'vietnam', 'oceania'].map(r => {return{name: r.replace("_", " "), value: r.replace(" ", "_")}})                      
