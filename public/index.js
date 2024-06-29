f57c23ed2bc8e31551faa5568f1128bbd50339f5
Glitch (valostatrak-bot) <none>
2024-06-29 00:09:39 +0000

    API Key errors fixed

--- public/index.js
+++ public/index.js
@@ -1,19 +1,24 @@
 const navButtons = document.querySelector("nav");
 const serverCount = document.querySelector("#servercount");
 // const clientServerCount = // host /servercount json;
-const clientServerCount = 758;
-let count = Number(serverCount.innerHTML[serverCount.innerHTML.length-1])
-let timer = 0;
 
-function changeCount() {
-    count += 1;
-    serverCount.innerHTML = "Server count: " + String(count);
-    if (count < clientServerCount) {
-        setTimeout(() => {
-            changeCount()
-        }, (100-clientServerCount)+timer)
-    }
-    timer++;
-}
+(async () => {
+  let clientServerCount = await fetch("/servercount")
+  clientServerCount = await clientServerCount.json()
+  clientServerCount = clientServerCount.count
+  let count = Number(serverCount.innerHTML[serverCount.innerHTML.length-1])
+  let timer = 0;
 
-// changeCount()
+  function changeCount() {
+      count += 1;
+      serverCount.innerHTML = "Server count: " + String(count);
+      if (count < clientServerCount) {
+          setTimeout(() => {
+              changeCount()
+          }, (100-clientServerCount)+timer)
+      }
+      timer++;
+  }
+
+  // changeCount() // Paused
+})()
